b584b74ebcd368601bca149aaa7a59c5b49c4c89
Hector276 <u201416126@upc.edu.pe>
2021-10-24 21:46:48 -0500

    Add: Material Design Modules and Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
+import {HomeComponent} from "./home/home.component";
+import {FinalProject2Component} from "./final-project2/final-project2.component";
 
+const routes: Routes = [
+  { path:'', component: HomeComponent },
+  { path:'Home', component: HomeComponent },
+  { path: 'FinaleProject', component: FinalProject2Component},
 ];
 
 @NgModule({
