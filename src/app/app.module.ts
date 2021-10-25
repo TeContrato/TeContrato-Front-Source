e229596259c75df887824536d704ed5c7d71322f
Julio-GM <u201721569@upc.edu.pe>
2021-10-24 23:15:09 -0500

    feat: Projects and controlmenu contractor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,58 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProjectsComponent } from './projects/pages/projects/projects.component';
+import { ControlMenuComponent } from './projects/pages/control-menu/control-menu.component';
+import { DialogComponent } from './projects/pages/dialog/dialog.component';
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatProgressBarModule} from "@angular/material/progress-bar";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatTooltipModule} from "@angular/material/tooltip";
+import {HttpClientModule} from "@angular/common/http";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatTableModule} from "@angular/material/table";
+import {AppRoutingModule} from "./app-routing.module";
+import {MatButtonModule} from "@angular/material/button";
+import {FormsModule} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
+import {MatCardModule} from "@angular/material/card";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsComponent,
+    ControlMenuComponent,
+    DialogComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    HttpClientModule,
+    MatTooltipModule,
+    MatSidenavModule,
+    MatProgressBarModule,
+    MatDialogModule
   ],
+  entryComponents:[DialogComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/projects/pages/dialog/dialog.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Inject} from '@angular/core';
+import {MatDialog} from "@angular/material/dialog";
+import { MatDialogRef, MAT_DIALOG_DATA} from "@angular/material/dialog";
+
+@Component({
+  selector: 'app-dialog',
+  templateUrl: './dialog.component.html',
+  styleUrls: ['./dialog.component.css']
+})
+export class DialogComponent implements OnInit {
+
+  constructor(public dialogRef: MatDialogRef<DialogComponent>,
+              @Inject(MAT_DIALOG_DATA) public message: string) {
+  }
+
+
+  ngOnInit(): void {
+  }
+
+  onClickNo(): void {
+    this.dialogRef.close();
+  }
+}
+
+
